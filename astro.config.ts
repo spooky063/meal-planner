993989afe12aa6abb95fe59b8859b7f4494f0227
Guillaume Astier <astier.gui@gmail.com>
2026-01-11 17:29:12 +0100

    chore: add netlify

--- astro.config.ts
+++ astro.config.ts
@@ -4,16 +4,22 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://spooky063.github.io',
   base: '/meal-planner',
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   integrations: [
     preact({
       include: ['**/preact/*'],
     }),
   ],
+
+  adapter: netlify(),
 });
